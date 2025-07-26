c9f7d3d98d1c9ab9dbf7e9523a851220852be419
mo <mo@zdvertex.com>
2025-07-26 22:38:48 +0800

    adjust styles

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 使用相對路徑，適合 GitHub Pages
+  build: {
+    outDir: 'docs', // 打包輸出到 docs 目錄
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['crypto-js', 'exceljs']
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
